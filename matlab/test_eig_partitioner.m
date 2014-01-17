e131a927912ba71951f1355b7cca354521398418
wwahby <w.wahby@gmail.com>
2014-01-17 15:41:08 -0500

    updated test_eig_partitioner to reflect new syntax (enabling multi-eig corrections)

--- matlab/test_eig_partitioner.m
+++ matlab/test_eig_partitioner.m
@@ -12,8 +12,8 @@ clear all
 % filename = 'netlists/p1.hgr';
 % filename_p = 'netlists/p1_add05.hgr';
 % filename_p = 'netlists/p1_del05a.hgr';
-filename = 'netlists/structP.hgr';
-filename_p = 'netlists/structP_add05.hgr';
+% filename = 'netlists/structP.hgr';
+% filename_p = 'netlists/structP_add05.hgr';
 % filename_p = 'netlists/structP_del05a.hgr';
 % filename = 'netlists/p2.hgr';
 % filename_p = 'netlists/p2_add05.hgr';
@@ -28,8 +28,8 @@ filename_p = 'netlists/structP_add05.hgr';
 % filename = 'netlists/ibm01.hgr';
 % filename_p = 'netlists/ibm01_add05.hgr';
 % filename_p = 'netlists/ibm01_del05a.hgr';
-% filename = 'netlists/industry3.hgr';
-% filename_p = 'netlists/industry3_add05.hgr';
+filename = 'netlists/industry3.hgr';
+filename_p = 'netlists/industry3_add05.hgr';
 % filename_p = 'netlists/industry3_del05a.hgr';
 %% Warning! Here be monsters. Have more than 3GB RAM free before you try to run these
 % filename = 'netlists/ibm10.hgr';
@@ -42,7 +42,7 @@ filename_p = 'netlists/structP_add05.hgr';
 
 
 %% Parameters
-num_eigs = 7; % number of eigenvalues and eigenvectors to use for the perturbed solution
+num_eigs = 10; % number of eigenvalues and eigenvectors to use for the perturbed solution
 node_areas = 1; % all nodes have unit area
 area_constraint = 1e-5; % schmoo across all possible area splits (set this intentionally low so we have a minimum of 1e-3% of the nodes on one side and the rest on the other, and try all partitions between that and 1-1e5)
 
@@ -50,23 +50,13 @@ area_constraint = 1e-5; % schmoo across all possible area splits (set this inten
 
 %% Run EIG to find exact partitioning results for original and perturbed systems
 [metrics times matrices eigs] = eig_partitioner(filename,num_eigs,node_areas,area_constraint); % normal
-[metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,2,node_areas,area_constraint); % exact perturbation, only need 2 eigenvalues here
+[metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,num_eigs,node_areas,area_constraint); % exact perturbation, only need 2 eigenvalues here
 
 
 
 %% Run the perturbed solver
-% If you want to run the perturbed solver WITHOUT running the full
-% partitioner for the perturbed solution, you'll need to read in the
-% netlist with the line below
-% [Qpe Dpe Ape] = parse_hgr_sparse_alt3(filename_p);
-
-% Construct perturbation matrices (i.e. the difference between the new
-% netlist and the old netlist)
-Qp = matrices_pe.laplacian - matrices.laplacian;
-Ape = matrices_pe.adjacency;
-[metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(filename_p,matrices.laplacian,eigs.vals,eigs.vecs,node_areas,area_constraint); % approx perturbation
-
-
+eigs_to_correct = 2;
+[metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(filename_p,matrices.laplacian,eigs.vals,eigs.vecs,node_areas,area_constraint,eigs_to_correct); % approx perturbation
 
 
 %% Full partitioning plots (here you can see the difference between a 40/60 split and a 60/50 split)
