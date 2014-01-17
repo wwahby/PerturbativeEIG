28a317629d83e8cc7c2111756b47030eac534a9a
wwahby <w.wahby@gmail.com>
2014-01-17 11:29:30 -0500

    Updated get_sorted_eigs to avoid sending bad eigs
    EIGS doesn't do that great for smallest eigenvalues. Should be able to
    use 'SM' or sigma=0 to do shift-invert mode to find the largest eigs of
    a transformed problem (which then correspond to the smallest eigs of our
    problem), but we frequently run into issues because 0 is an eigenvalue
    of the original problem. Instead we're using sigma=-1, and instead of
    iterating forever to make sure we have good eigs, we'll accept some bad
    ones, throw away all the negative ones, and throw away all of the tiny
    ones except one. That process should at least ensure that the 2nd and
    higher eigs are mostly correct, though the actual ground state eig may
    still be spurious. Since the ground state eig is just a constant value
    (and since the spurious eigs are still normalized) it probably shouldn't
    affect us TOO much.

--- matlab/get_sorted_eigs.m
+++ matlab/get_sorted_eigs.m
@@ -1,11 +1,14 @@
 function [vals vecs] = get_sorted_eigs(Q,num_eigs)
 % return 1xn vector of sorted eigenvalues
 % and eigenvectors as column vectors in the same order as the eigenvalues
+% May not return num_eigs values, as some may be spurious negative eigs
 
 if issparse(Q)
-    opts.issym = 1; % The laplacian is symmetric, so use some optimizations
-    opts.isreal = 1; % The laplacian is real, so use some optimizations
+    opts.issym = true; % The laplacian is symmetric, so use some optimizations
+    opts.isreal = true; % The laplacian is real, so use some optimizations
     opts.disp = 0; % don't spam the console with info about the solution
+%     opts.tol = 1e-20; % really fine tolerance
+%     opts.maxit = 3e4; % lots of iterations
     [vecs vals] = eigs(Q,num_eigs,-1,opts); % using -1 because in some cases 'SM' or 0 will cause poorly-conditioned matrices during LU factorization
     % This seems to be happening because eigs uses our input as a shift value during factorization (or something)
     % -1 should still be fine since all eigenvalues should be >= 0, so
@@ -20,3 +23,23 @@ vals = diag(vals); % just get nx1 vector of eigenvalues
 
 [vals order] = sort(vals); % get sorted eigenvalues and their order
 vecs = vecs(:,order); % get eigenvectors in sorted eigenvalue order
+
+%% throw away negative eigenvalues 
+% MATLAB may give us spurious negative eigenvalues -- use this to throw
+% them away
+% Best method would be to use 'SM' and shift-invert mode to find actual smallest
+% eigenvalues, but it occasionally fails because 0 is an eigenvalue
+posvals = (vals > 0);
+vals = vals(posvals);
+vecs = vecs(:,posvals);
+
+%% Throw away really tiny positive values, as they're probably spurious too
+bigvals = (vals > 1e-5);
+
+% we need to keep the first "zero" eigenvalue
+if(bigvals(1) == 0)
+    ind = find(bigvals == 0,1,'last');
+    bigvals(ind) = 1;
+end
+vals = vals(bigvals);
+vecs = vecs(:,bigvals);
