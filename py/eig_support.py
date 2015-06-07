28d05fbee90ec058e8b30c9e91d547d5a2663066
wwahby <w.wahby@gmail.com>
2015-06-06 22:23:00 -0700

    Fixed perturbed partitioning

--- py/eig_support.py
+++ py/eig_support.py
@@ -1,7 +1,6 @@
 import re	# regex
-import sys	# for grabbing input args
 import numpy as np	# array handling
-import numpy.linalg as la	# eigenvalues
+import scipy.linalg as la	# eigenvalues
 
 def parse_hgr(infile_name,delim):
 	# Read input file
@@ -41,6 +40,47 @@ def parse_hgr(infile_name,delim):
 
 	return Q
 
+def parse_hgr_sparse(infile_name,delim):
+	# [FIX]  -- NEED TO ACTUALLY IMPLEMENT SPARSE MATRICES -- SO FAR JUST COPIED THE OTHER METHOD
+	# Read input file
+	infile = open(infile_name,'r')
+	lines = infile.readlines()
+	infile.close()
+
+	# strip off trailing whitespace
+	for i in range(len(lines)):
+		lines[i] = lines[i].rstrip()
+
+	# get number of nets and cells
+	n = lines[0].split(delim)
+	num_nets = int(n[0])
+	num_cells = int(n[1])
+
+	# [FIX] Use sparse matrix handling
+	Q = np.zeros((num_cells,num_cells)) # initialize laplacian matrix
+
+	## Process the HGR file
+	for net in range(1,len(lines)):	# iterate over all nets (ignore first, as that just gives us number of cells and nets)
+		line_arr = lines[net].split(delim)	# Break up
+		
+		if(len(line_arr) > 1): # Only count nets that have more than one node
+			net_cells = len(line_arr)
+			net_weight = 1/(net_cells-1)
+			#print(net_weight)
+
+			for i in range(len(line_arr)):
+				i_cell = int(line_arr[i])-1 # -1 since hgr file starts with index 1, and python starts with index 0
+				Q[i_cell,i_cell] = Q[i_cell,i_cell] + net_weight*(net_cells-1) # Add up connected net weights on diagonals (constructing D)
+
+				for j in range(i+1,len(line_arr)):
+					j_cell = int(line_arr[j])-1
+					Q[i_cell,j_cell] = Q[i_cell,j_cell] - net_weight
+					Q[j_cell,i_cell] = Q[j_cell,i_cell] - net_weight
+
+	return Q
+
+
+
 def partition_1d(Q):
 	(vals, vecs) = la.eigh(Q) # Q is guaranteed to be hermitian since it is a real symmetric matrix
 	sorted_vals = np.argsort(vals)
@@ -57,53 +97,27 @@ def partition_1d_perturbed(Q,Qp,n):
 	sorted_vals = np.argsort(vals)
 
 	#vals = np.matrix(vals)
-	vecs = np.matrix(vecs)
+	#vecs = np.matrix(vecs)
 	
 	val2 = vals[sorted_vals[1]]
 	vec2 = vecs[:,sorted_vals[1]]
 
 	vecs_to_include = [0] + list(range(2,n))
 
-	vec_approx = np.matrix(np.zeros((np.size(vec2),1)))
-	Qp = np.matrix(Qp)
-
-	for i in vecs_to_include:
-		vv = vecs[:,sorted_vals[i]]
-
-		#top = vv.T * (Qp * vec2)
-		top = Qp * vec2
-		top = vv.T*top
-		top = float(top)
-		bot = val2 - vals[sorted_vals[i]]
-		vec_approx = vec_approx + (top*vv)/bot
-
-	partition1d = np.argsort(list(vec_approx.T))
+	vec_approx = np.zeros((np.size(vec2),1))
+	Qp = np.array(Qp)
 	
-	return partition1d
-
-def partition_1d_perturbed_alt(Q,Qp,n):
-	(vals, vecs) = la.eigh(Q) # Q is guaranteed to be hermitian since it is a real symmetric matrix
-	sorted_vals = np.argsort(vals)
-	
-	val2 = vals[sorted_vals[1]]
-	vec2 = vecs[:,sorted_vals[1]]
-
-	vecs_to_include = [0] + list(range(2,n))
-
-	vec_approx = np.matrix(np.zeros((np.size(vec2),1)))
-	Qp = np.matrix(Qp)
-
 	for i in vecs_to_include:
-		vv = vecs[:,sorted_vals[i]]
+		vv = np.zeros( (np.size(vec2),1) )
+		vv[:,0] = vecs[:,sorted_vals[i]]
 
-		#top = vv.T * (Qp * vec2)
-		top = Qp * vec2
-		top = vv.T*top
-		top = float(top)
+		top = float( (vv.T).dot( Qp.dot(vec2) ) )
 		bot = val2 - vals[sorted_vals[i]]
-		vec_approx = vec_approx + (top*vv)/bot
+		vec_approx = vec_approx + (top/bot)*vv
 
-	partition1d = np.argsort(list(vec_approx.T))
+	partition1d = np.argsort((vec_approx.T))
+	partition1d = list(partition1d[0]) # Converting this back to a list. the array access is because the NP ndarray is 10x1 and
+									   # we just want a normal "dimensionless" 10 element list
 	
 	return partition1d
 
@@ -116,17 +130,15 @@ def compare_lists(A,B,split_ratio):
 	# determine index to split at
 	split_ind = int(round(split_ratio*len(A)))
 	A1 = set(A[0:split_ind])
+	A2 = set(A[split_ind:])
 	B1 = set(B[0:split_ind])
 	B2 = set(B[split_ind:])
 
 	first_half_diff = A1.difference(B1)
 	sec_half_diff = A1.difference(B2)
 
-	print(A)
-	print(A1)
-	print(split_ind)
 	mismatch_ratio_1 = len(first_half_diff)/len(A1)
-	mismatch_ratio_2 = len(sec_half_diff)/len(A2)
+	mismatch_ratio_2 = len(sec_half_diff)/len(A1)
 
 	return (mismatch_ratio_1,mismatch_ratio_2)
 
