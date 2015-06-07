6620323555e7206a148180cd2ed50f57976d6d1b
wwahby <w.wahby@gmail.com>
2015-06-06 22:49:33 -0700

    Improved eig_partition wrapper input handling

--- py/eig_partition.py
+++ py/eig_partition.py
@@ -5,46 +5,49 @@ import eig_support as es
 import argparse
 
 
-#def main():
-#	parser = argparse.ArgumentParser()
-#	parser.add_argument("infile", type=float, help="Name of input HGR file")
-#	parser.add_argument("k", type=float, help="Rent constant for the design")
-#	parser.add_argument("p", type=float, help="Rent exponent for the design")
-#	parser.add_argument("tiers", type=int, help="Number of tiers in the 3D stack")
-#	args = parser.parse_args()
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("hgr", help="Name of input HGR file", nargs='+') # '+' so we can reconstruct filenames with spaces
+	parser.add_argument("--perturbed", "-p", help="Name of the perturbed HGR file", nargs='+')
+	parser.add_argument("--num_eigs", "-n", type=int, help="Number of eigenvectors to use for perturbation approximation", default=10)
+	parser.add_argument("--delim", "-d", help="Delimeter used in netlist files", default = " ")
+	args = parser.parse_args()
 
-delim = " "	# input file delimeter. Must be a single character
+	infile_name = ' '.join(args.hgr) # Reconstruct filenames with spaces
+	if (args.perturbed):
+		infile_p_name = ' '.join(args.perturbed) # Reconstruct filenames with spaces
+		run_perturbed = True
+	else:
+		infile_p_name = "Invalid_name"
+		run_perturbed = False
 
-# Grab command line arguments
-#infile_name = sys.argv[1]
-infile_name = "p2.hgr"
-infile_p_name = "p2_p.hgr"
+	num_eigs = args.num_eigs
+	delim = args.delim	# input file delimeter. Must be a single character
 
-Q = es.parse_hgr(infile_name,delim)
+	Q = es.parse_hgr(infile_name,delim)
+	p1d = es.partition_1d(Q)
 
-#np.savetxt("testout.txt",Q,fmt='%.4f',delimiter='\t',newline='\n')
-
-p1d = es.partition_1d(Q)
-
-print("Exact partition\n==============\n" + str(p1d))
-print()
-
-#if(len(sys.argv) > 2):
-	#infile_p_name = sys.argv[2]
+	print("Exact partition\n==============\n" + str(p1d))
+	print()
 
 
-n = 10
-Qp_exact = es.parse_hgr(infile_p_name,delim)
-Qp = Qp_exact - Q
-p1dp = es.partition_1d_perturbed(Q,Qp,n)
-p1dpe = es.partition_1d(Qp_exact)
+	if (run_perturbed):
+		
+		Qp_exact = es.parse_hgr(infile_p_name,delim)
+		Qp = Qp_exact - Q
+		p1dp = es.partition_1d_perturbed(Q,Qp,num_eigs)
+		p1dpe = es.partition_1d(Qp_exact)
 
 
-print("Exact perturbed partition\n==============\n" + str(p1dpe))
-print()
-print("Approx perturbed partition\n==============\n" + str(np.array(p1dp)))
-print()
+		print("Exact perturbed partition\n==============\n" + str(p1dpe))
+		print()
+		print("Approx perturbed partition\n==============\n" + str(np.array(p1dp)))
+		print()
 
 
-(r1,r2) = es.compare_lists(list(p1dp),list(p1dpe),0.5)
-print("{0:>6.4f}\t{1:6.4f}".format(r1,r2))
+		(r1,r2) = es.compare_lists(list(p1dp),list(p1dpe),0.5)
+		print("{0:>6.4f}\t{1:6.4f}".format(r1,r2))
+
+
+if ( __name__ == "__main__"):
+	main()
