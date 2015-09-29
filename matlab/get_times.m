e132675b635c232bc3a05096b9a103f4176e04e7
wwahby <w.wahby@gmail.com>
2015-09-29 15:48:21 -0400

    Updated matlab version of perturbation comparison to also plot cutsize/ratio cut stability over multiple runs

--- matlab/get_times.m
+++ matlab/get_times.m
@@ -1,6 +1,6 @@
 % test eig_partitioner function
-filename = 'p1.hgr';
-filename_p = 'p1_add05.hgr';
+filename = '../netlists/biomedP.hgr';
+filename_p = '../netlists/biomedP_add05.hgr';
 num_eigs = 7;
 node_areas = 1; % all nodes have unit area
 area_constraint = 0.55; % schmoo across all possible area splits (set this intentionally low so we have a minimum of 1e-3% of the nodes on one side and the rest on the other, and try all partitions between that and 1-1e5)
@@ -28,19 +28,44 @@ tp_eigs = zeros(1,iterations);
 tp_tot = zeros(1,iterations);
 tp_tot_with_parse = zeros(1,iterations);
 
+rc_vec_o = zeros(1,iterations);
+mc_vec_o = zeros(1,iterations);
+skew_rc_vec_o = zeros(1,iterations);
+skew_mc_vec_o = zeros(1,iterations);
+
+rc_vec_pe = zeros(1,iterations);
+mc_vec_pe = zeros(1,iterations);
+skew_rc_vec_pe = zeros(1,iterations);
+skew_mc_vec_pe = zeros(1,iterations);
+
+rc_vec_pa = zeros(1,iterations);
+mc_vec_pa = zeros(1,iterations);
+skew_rc_vec_pa = zeros(1,iterations);
+skew_mc_vec_pa = zeros(1,iterations);
+
 %% Run a bunch of times
 
 for i= 1:iterations
     
     repstr = sprintf('Iteration %d of %d',i,iterations);
     disp(repstr);
+    %% Run EIG to find exact partitioning results for original and perturbed systems
     [metrics times matrices eigs] = eig_partitioner(filename,num_eigs,node_areas,area_constraint); % normal
-    [metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,2,node_areas,area_constraint); % exact perturbation
+    [metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,num_eigs,node_areas,area_constraint); % exact perturbation, only need 2 eigenvalues here
 
-    Qp = matrices_pe.laplacian - matrices.laplacian;
-    Ape = matrices_pe.adjacency;
-    [metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(Qp,Ape,eigs.vals,eigs.vecs,node_areas,area_constraint); % approx perturbation
+
+    %% Run the perturbed solver
+    eigs_to_correct = 2;
+    [metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(filename_p,matrices.laplacian,eigs.vals,eigs.vecs,node_areas,area_constraint,eigs_to_correct); % approx perturbation
+
+    mc_vec_o(i) = metrics.cutsize.min;
+    mc_vec_pe(i) = metrics_pe.cutsize.min;
+    mc_vec_pa(i) = metrics_p.cutsize.min;
     
+    rc_vec_o(i) = metrics.ratio_cut.min;
+    rc_vec_pe(i) = metrics_pe.ratio_cut.min;
+    rc_vec_pa(i) = metrics_p.ratio_cut.min;
+
     t_par(i) = times.partition;
     t_place(i) = times.placement;
     t_parse(i) = times.parse;
@@ -76,4 +101,32 @@ xlabel('time (s)')
 ylabel('Cumulative distribution')
 title('Time distribution (industry3, 20 runs, 5% skew)')
 fixfigs(1,3,14,12)
+set(gca,'xscale','log')
+
+figure(2)
+clf
+h1 = cdfplot(mc_vec_o);
+hold on
+h2 = cdfplot(mc_vec_pe);
+h3 = cdfplot(mc_vec_pa);
+set(h1,'color','k')
+set(h2,'color','b')
+set(h3,'color','r')
+xlabel('Mincut')
+ylabel('Cumulative distribution')
+fixfigs(2,3,14,12)
+set(gca,'xscale','log')
+
+figure(3)
+clf
+h1 = cdfplot(rc_vec_o);
+hold on
+h2 = cdfplot(rc_vec_pe);
+h3 = cdfplot(rc_vec_pa);
+set(h1,'color','k')
+set(h2,'color','b')
+set(h3,'color','r')
+xlabel('Ratio Cut')
+ylabel('Cumulative distribution')
+fixfigs(3,3,14,12)
 set(gca,'xscale','log')
--- matlab/get_variance.m
+++ matlab/get_variance.m
@@ -1,31 +1,32 @@
 % test eig_partitioner function
 %% Choose input file
-% filename = 'ckt_f216.hgr';
-% filename_p = 'ckt_f216_p.hgr';
-% filename = 'fract.hgr';
-% filename_p = 'fract_ptest.hgr';
-% filename = 'p1.hgr';
-% filename_p = 'p1_add05.hgr';
-% filename = 'structP.hgr';
-% filename_p = 'structP_add05.hgr';
-% filename = 'p2.hgr';
-% filename_p = 'p2_add05.hgr';
-% filename = 'biomedP.hgr';
-% filename_p = 'biomedP_add05.hgr';
-% filename_p = 'biomedP_add005.hgr';
-filename = 'industry2.hgr';
-filename_p = 'industry2_add05.hgr';
-% filename = 'ibm01.hgr';
-% filename_p = 'ibm01_add05.hgr';
-% filename = 'industry3.hgr';
-% filename_p = 'industry3_add05.hgr';
-% filename = 'ibm10.hgr';
-% filename_p = 'ibm10_add05.hgr';
-% filename = 'ibm18.hgr';
-% filename_p = 'ibm18_add05.hgr';
+% filename = '../netlists/ckt_f216.hgr';
+% filename_p = '../netlists/ckt_f216_p.hgr';
+% filename = '../netlists/fract.hgr';
+% filename_p = '../netlists/fract_ptest.hgr';
+% filename = '../netlists/p1.hgr';
+% filename_p = '../netlists/p1_add05.hgr';
+% filename = '../netlists/structP.hgr';
+% filename_p = '../netlists/structP_add05.hgr';
+filename = '../netlists/p2.hgr';
+filename_p = '../netlists/p2_add05.hgr';
+% filename = '../netlists/biomedP.hgr';
+% filename_p = '../netlists/biomedP_add05.hgr';
+% filename_p = '../netlists/biomedP_add005.hgr';
+% filename = '../netlists/industry2.hgr';
+% filename_p = '../netlists/industry2_add05.hgr';
+% filename = '../netlists/ibm01.hgr';
+% filename_p = '../netlists/ibm01_add05.hgr';
+% filename = '../netlists/industry3.hgr';
+% filename_p = '../netlists/industry3_add05.hgr';
+% filename = '../netlists/ibm10.hgr';
+% filename_p = '../netlists/ibm10_add05.hgr';
+% filename = '../netlists/ibm18.hgr';
+% filename_p = '../netlists/ibm18_add05.hgr';
+
 
 %% Partitioning parameters
-num_eigs = 7;
+num_eigs = 10;
 node_areas = 1; % all nodes have unit area
 area_constraint = 1e-5; % schmoo across all possible area splits (set this intentionally low so we have a minimum of 1e-3% of the nodes on one side and the rest on the other, and try all partitions between that and 1-1e5)
 iterations = 20;
@@ -36,13 +37,15 @@ for i= 1:iterations
     
     repstr = sprintf('Iteration %d of %d',i,iterations);
     disp(repstr);
+    %% Run EIG to find exact partitioning results for original and perturbed systems
     [metrics times matrices eigs] = eig_partitioner(filename,num_eigs,node_areas,area_constraint); % normal
-    [metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,2,node_areas,area_constraint); % exact perturbation
+    [metrics_pe times_pe matrices_pe eigs_pe] = eig_partitioner(filename_p,num_eigs,node_areas,area_constraint); % exact perturbation, only need 2 eigenvalues here
 
-    Qp = matrices_pe.laplacian - matrices.laplacian;
-    Ape = matrices_pe.adjacency;
-    [metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(Qp,Ape,eigs.vals,eigs.vecs,node_areas,area_constraint); % approx perturbation
-    
+
+    %% Run the perturbed solver
+    eigs_to_correct = 2;
+    [metrics_p times_p matrices_p eigs_p] = eig_partitioner_perturbed(filename_p,matrices.laplacian,eigs.vals,eigs.vecs,node_areas,area_constraint,eigs_to_correct); % approx perturbation
+
     if (i == 1) % initialize all our arrays
         rcv = zeros(iterations,length(metrics.ratio_cut.vec));
         cv = zeros(iterations,length(metrics.ratio_cut.vec));
