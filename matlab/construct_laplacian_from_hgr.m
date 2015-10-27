1405e024bb6526888bfd6cb590ed4c821afa4ad0
wwahby <w.wahby@gmail.com>
2015-10-27 12:35:32 -0400

    eig calculation will now be redone if insufficient real eigenvalues are found

--- matlab/construct_laplacian_from_hgr.m
+++ matlab/construct_laplacian_from_hgr.m
@@ -1,13 +1,23 @@
-function [Q vals vecs D A time_parse time_eig] = construct_laplacian_from_hgr(filename,num_eigs)
+function [Q, vals, vecs, D, A, time_parse, time_eig] = construct_laplacian_from_hgr(filename,num_eigs)
 
 tic
 % read in the netlist
-[Q D A] = parse_hgr_sparse_alt3(filename);
+[Q, D, A] = parse_hgr_sparse_alt3(filename);
 time_parse = toc;
 %disp('...HGR Parsing done!')
 
-tic
-% Get the sorted eigenvalues and eigenvectors
-[vals vecs] = get_sorted_eigs(Q,num_eigs);
-time_eig = toc;
-%disp('...Eigenvalues found!')
+sufficient_vals = 0;
+num_runs = 0;
+max_runs = 10;
+
+while ((sufficient_vals == 0) && (num_runs < max_runs))
+    % Get the sorted eigenvalues and eigenvectors
+    tic
+    [vals, vecs] = get_sorted_eigs(Q,num_eigs);
+    time_eig = toc;
+
+    if (length(vals) > 2)
+        sufficient_vals = 1;
+    end
+    num_runs = num_runs + 1;
+end
